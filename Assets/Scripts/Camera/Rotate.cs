22f267e4e8ce0610ae038e065626559c8ca4e704
HenryAM <34387461+HenryAM@users.noreply.github.com>
2017-12-09 23:29:36 -0800

    Add files via upload

---
+++ Assets/Scripts/Camera/Rotate.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+    public float Speed = 10f;
+
+	void Update () {
+        transform.Rotate(Vector3.up, Speed * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/Camera/TargetViewer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetViewer : MonoBehaviour {
+
+    public Transform watchTarget;
+	void Update () {
+        transform.LookAt(watchTarget);
+	}
+}
